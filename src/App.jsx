0f2252de2b8692437703da68e4dbab566a1350e6
bvenceslas <bvenceslas@gmail.com>
2025-01-25 17:52:22 +0200

    use redux hooks useSelector and useDispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 
 function App() {
   return (
     <>
       <div>
         <h1 className="text-red-700">Welcome to cake shop</h1>
+        <p className="text-white mt-5 text-center">Hooks Above</p>
+        <HooksCakeContainer />
+        <p className="text-red-700 mt-10 text-center">Connect Below</p>
         <CakeContainer />
       </div>
     </>
---
+++ src/components/HooksCakeContainer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { buyCake } from "../redux";
+
+const HooksCakeContainer = (props) => {
+  const dispatch = useDispatch();
+  const numOfCakes = useSelector((state) => state.numOfCakes);
+
+  return (
+    <div className="w-[500px] mt-10 p-5 border border-amber-700 rounded-2xl">
+      <h2 className="mb-5">Number of Cake - {numOfCakes}</h2>
+      <button onClick={() => dispatch(buyCake())}>Buy A Cake</button>
+    </div>
+  );
+};
+
+export default HooksCakeContainer;
