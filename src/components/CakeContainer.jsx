9665c6af444aa7cf547aa79dadeab683f249e33c
bvenceslas <bvenceslas@gmail.com>
2025-01-25 17:10:46 +0200

    dispatching buy cake

--- src/components/CakeContainer.jsx
+++ src/components/CakeContainer.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-const CakeContainer = () => {
+import { buyCake } from "../redux";
+import { connect } from "react-redux";
+const CakeContainer = (props) => {
   return (
-    <div className="w-[500]px mt-10 p-5 border border-gray-50 rounded-2xl">
-      <h2 className="mb-5">Number of Cake - ?</h2>
-      <button>Buy A Cake</button>
+    <div className="w-[500px] mt-10 p-5 border border-gray-50 rounded-2xl">
+      <h2 className="mb-5">Number of Cake - {props.numOfCakes}</h2>
+      <button onClick={props.buyCake}>Buy A Cake</button>
     </div>
   );
 };
 
-export default CakeContainer;
+const mapStateToProps = (state) => {
+  return {
+    numOfCakes: state.numOfCakes,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCake: () => dispatch(buyCake()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
--- src/redux/cake/cakeActions.js
+++ src/redux/cake/cakeActions.js
@@ -1,8 +1,9 @@
 import { BUY_CAKE } from "./cakeTypes";
 
 export const buyCake = () => {
+  console.log("object");
   return {
     type: BUY_CAKE,
-    payload: {},
+    // payload: {},
   };
 };
