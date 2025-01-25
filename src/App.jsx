c4d3bc2485c223a04c8a525dfc565acb29205405
bvenceslas <bvenceslas@gmail.com>
2025-01-25 16:48:16 +0200

    create cake state, action, reducer and type

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import CakeContainer from "./components/CakeContainer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
         <h1 className="text-red-700">Welcome to cake shop</h1>
+        <CakeContainer />
       </div>
     </>
   );
---
+++ src/components/CakeContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const CakeContainer = () => {
+  return (
+    <div className="w-[500]px mt-10 p-5 border border-gray-50 rounded-2xl">
+      <h2 className="mb-5">Number of Cake - ?</h2>
+      <button>Buy A Cake</button>
+    </div>
+  );
+};
+
+export default CakeContainer;
